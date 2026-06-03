305514746b971b0a42604cc562d7ab7bf4fd49fc
Jay Barbhaiya <jay.barbhaiya@sap.com>
2026-06-03 11:17:49 +0200

    add light background

--- modules/appearance.lua
+++ modules/appearance.lua
@@ -1,8 +1,8 @@
 local wezterm = require("wezterm")
 
 return {
-    -- color_scheme = "Catppuccin Latte",
-    -- color_scheme = "Vs Code Light+ (Gogh)",
-    font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
-    font_size = 14,
+	-- color_scheme = "Catppuccin Latte",
+	color_scheme = "Vs Code Light+ (Gogh)",
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
+	font_size = 14,
 }
